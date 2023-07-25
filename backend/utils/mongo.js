a8501d9fa963b82e6a24f016ac09a4c024761c73
Ryan Hardin <rhardin378@gmail.com>
2023-07-24 22:46:22 -0400

    working exercise model with make make exercise and add set helper functions

--- backend/utils/mongo.js
+++ backend/utils/mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const { Schema } = mongoose;
 
 if (process.argv.length<3) {
     console.log('give password as argument')
@@ -16,11 +16,68 @@ mongoose.connect(url).then(()=> {
     console.log(`connected to DB on ${url}`)
 })
 
-//define a schema 
+//define a schema
+
+//set schema
+// const setSchema = new Schema({
+//     reps: Number,
+//     weight: Number,
+// })
+//workout schema
+// const workoutSchema = new Schema ({
+//     name: String,
+//     exercises: [{type: Schema.Types.ObjectId, ref:"Exercise"}]
+// })
+
+
+
+// measurements schema
+
+//Journal schema
+
 
 
 //define a model
+// const Set = mongoose.model('Set', setSchema)
 
+//exercise schema
+const exerciseSchema = new Schema({
+    name: String,
+    bodyPart: String, 
+    sets: [   { reps: Number,
+        weight: Number,
+    }],    
+    })
+
+
+
+const Exercise = mongoose.model('Exercise', exerciseSchema)
+
+
+const makeExercise = async() => {
+    const exercise = new Exercise({ name: 'Leg Press', bodyPart: 'Legs' })
+    exercise.sets.push({reps: 10, weight: 100 })
+const res = await exercise.save()
+console.log(res)
+}
+
+const addSet = async() => { 
+const exercise = await Exercise.findOne({name: 'Leg Press'})
+exercise.sets.push({reps: 13, weight: 103})
+const res = await exercise.save()
+console.log(res)
+}
+
+// makeExercise()
+addSet()
+
+
+// Exercise.find({}).then(result=> {
+//     result.forEach(exercise=> {
+//         console.log(`${exercise.name}`)  
+//     } )
+// }
+//     )
 // const Note = mongoose.model('Note', noteSchema)
 
 //create new workout object in a variable to save
